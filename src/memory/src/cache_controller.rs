298f98f658d99d38d0f0ed254e280992bbdabf00
puddo <davifpuddo@gmail.com>
2026-07-22 14:10:30 -0300

    feat(cache): initialization of the unique policy lists + function to find the next block to be substituted according to the policy

--- src/memory/src/cache_controller.rs
+++ src/memory/src/cache_controller.rs
@@ -61,6 +61,9 @@ pub struct CacheLevel{
     policy: CachePolicy,
 
     #[serde(skip)]
+    policy_list: Vec<usize>,    // List used for LRU, LFU and FIFO logic 
+
+    #[serde(skip)]
     offset_mask: usize,
     #[serde(skip)]
     index_mask: usize,
@@ -98,8 +101,16 @@ impl From<CacheConfig> for CacheLevel {
         let index_mask = mask_from!(index_size, offset_size);
         let tag_mask = (offset_mask | index_mask) ^ usize::MAX; // remaining bits
 
+        let policy_list : Vec<usize> = match config.policy {
+            CachePolicy::LRU => (0..config.set_size).rev().collect(),
+            CachePolicy::LFU => vec![0; config.set_size],
+            CachePolicy::FIFO => vec![],
+            CachePolicy::Random => vec![],
+        };
+
         CacheLevel {
             policy: config.policy,
+            policy_list,
             index_mask, 
             tag_mask, 
             offset_mask,
@@ -127,8 +138,16 @@ impl CacheLevel {
         let index_mask = mask_from!(index_size, offset_size);
         let tag_mask = (offset_mask | index_mask) ^ usize::MAX; // remaining bits
 
+        let policy_list : Vec<usize> = match policy {
+            CachePolicy::LRU => (0..associativity).rev().collect(),
+            CachePolicy::LFU => vec![0; associativity],
+            CachePolicy::FIFO => vec![],
+            CachePolicy::Random => vec![],
+        };
+
         CacheLevel {
             policy,
+            policy_list,
             index_mask,
             tag_mask,
             offset_mask,
@@ -143,18 +162,58 @@ impl CacheLevel {
         }
     }
 
+    fn policy_next(&mut self) -> usize {
+        match self.policy {
+            CachePolicy::LRU => { 
+                match self.policy_list.pop() {
+                    Some(res) => {
+                        self.policy_list.insert(0, res);
+                        res
+                    }
+                    None => {
+                        eprintln!("Finding next position from the policy list has failed. 
+                            Defaulting to 0");
+                        0
+                    }
+                }
+            },
+            CachePolicy::LFU => {
+                match self.policy_list.iter().min() {
+                    Some(&min) => min,
+                    None => {
+                        eprintln!("Finding next position from the policy list has failed. 
+                            Defaulting to 0");
+                        0
+                    }
+                }
+            },
+            CachePolicy::FIFO => {
+                match self.policy_list.pop() {
+                    Some(res) => res,
+                    None => {
+                        if self.policy_list.len() != 0 {
+                            eprintln!("Finding next position from the policy list has failed. 
+                                Defaulting to 0");
+                        }
+                        0
+                    }
+                }
+            },
+            CachePolicy::Random => random_range(0..self.way)
+        }
+    }
+
+    fn policy_update(&mut self, idx: usize) {
+        todo!();
+    }
+
     // General insertion method
     pub fn insert(&mut self, addr: usize, data: Vec<u8>, set_dirty: bool) -> bool {
         let tmp = (addr & self.index_mask) >> self.index_start;
         let idx = (tmp % self.n_sets) * self.way;
 
-        let i = match self.policy {
-            CachePolicy::LRU => 0,
-            CachePolicy::LFU => 0,
-            CachePolicy::FIFO => 0,
-            CachePolicy::Random => random_range(0..self.way)
-        };
-
+        let i = self.policy_next();
+        
         match self.data.get(idx + i) {
             Some(_) => {
                 self.data[idx + i].update(data, addr & self.offset_mask);
@@ -163,6 +222,7 @@ impl CacheLevel {
                 if set_dirty {
                     self.dirty[idx + i] = true;
                 }
+                self.policy_update(idx + i);
                 return true
             },
             _ => eprintln!("Insertion failed! Could not index position: {}", idx + i)
@@ -171,7 +231,7 @@ impl CacheLevel {
     }
 
     // Returns the bytes in this cache level
-    pub fn read_level(&self, addr: usize, bytes: usize) -> CacheReturn {
+    pub fn read_level(&mut self, addr: usize, bytes: usize) -> CacheReturn {
         let tmp = (addr & self.index_mask) >> self.index_start;
         let idx = (tmp % self.n_sets) * self.way;
 
@@ -179,13 +239,14 @@ impl CacheLevel {
         
         for i in 0..self.way {
             match (self.valid.get(idx + i), self.tags.get(idx + i)) {
-                (Some(&true), Some(&value)) => {
-                    if value == tag {
-                        let offset = addr & self.offset_mask;
-                        let block = self.data[idx + i].clone();
-                        let slice = &block.bytes[offset..bytes];
-                        return CacheReturn::Hit(slice.to_vec());
-                    }
+                (Some(&true), Some(&value)) if value == tag => {
+                    let offset = addr & self.offset_mask;
+                    let block = self.data[idx + i].clone();
+                    let slice = &block.bytes[offset..bytes];
+
+                    self.policy_update(idx + i);
+
+                    return CacheReturn::Hit(slice.to_vec());
                 },
                 (None, _) | (_, None) => return CacheReturn::Error,
                 _ => (),
