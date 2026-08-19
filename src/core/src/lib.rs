7e196474f300ec50a55bb2f823c7967772c9fd54
mateushmd <mateushmdiniz@gmail.com>
2026-08-18 23:43:29 -0300

    core: refactored schedule function

--- src/core/src/lib.rs
+++ src/core/src/lib.rs
@@ -1,11 +1,16 @@
-use crate::event::Event;
+use crate::event::{Event, EventPayload};
 
 pub mod event;
 
 pub type ModuleId = usize;
 
+pub enum Target {
+    Module(ModuleId),
+    Myself
+}
+
 pub trait EngineContext {
-    fn schedule(&mut self, event: Event);
+    fn schedule(&mut self, timestamp: u64, target: Target, payload: EventPayload);
 }
 
 pub trait Module { 
