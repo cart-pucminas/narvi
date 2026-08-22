4443f6aabc9a033097aeb8ffd7123078cf6c2109
mateushmd <mateushmdiniz@gmail.com>
2026-08-22 14:30:30 -0300

    fix(engine): fixed hit/miss registration

--- src/engine/src/lib.rs
+++ src/engine/src/lib.rs
@@ -59,18 +59,17 @@ impl<'a> EngineContext for ActiveContext<'a> {
         // TODO: no need for HartJournal and CacheJournal
         // keeping for now because I'm lazy
 
-        let cache_level = self.cache_level_map.get(&self.current_module_id)
-            .expect("could not find level");
+        let cache_level = self.cache_level_map.get(&self.current_module_id);
 
         let mut hart_journal = HartJournal::new();
         let mut cache_journal = CacheJournal::new(self.cache_level_map.len());
 
         match event {
             JournalEvent::CacheHit => {
-                cache_journal.hit(*cache_level);
+                cache_journal.hit(*cache_level.expect(&format!("could not find level with id {}", self.current_module_id)));
             },
             JournalEvent::CacheMiss => {
-                cache_journal.miss(*cache_level);
+                cache_journal.miss(*cache_level.expect(&format!("could not find level with id {}", self.current_module_id)));
             },
             JournalEvent::Cycles { cycles } => {
                 hart_journal.cycles_done(cycles as u128);
@@ -101,11 +100,6 @@ impl Engine {
         let mut modules: Vec<Box<dyn Module>> = Vec::new();
         let mut cache_level_map: HashMap<ModuleId, usize> = HashMap::new();
 
-        for _ in 0..config.hart_count {
-            let mut hart = Hart::from_extensions(&config.extensions);
-            modules.push(Box::new(hart));
-        }
-
         let ram = {
             let mut ram = Ram::new(config.ram_size);
             ram.write_bytes(0, assembly);
@@ -121,34 +115,64 @@ impl Engine {
             cache.set_backing_store(previous_store_id);
             previous_store_id = modules.len();
             modules.push(Box::new(cache));
+            cache_level_map.insert(previous_store_id, level);
         }
+
+        for _ in 0..config.hart_count {
+            let mut hart = Hart::from_extensions(&config.extensions, previous_store_id);
+            modules.push(Box::new(hart));
+        }
+
 
         let cache_levels = cache_level_map.len();
 
-        Self {
+        let mut engine = Self {
             modules,
             event_queue: Default::default(),
             time: 0,
             cache_level_map,
             journal: Journal::new(cache_levels)
-        }
+        };
+        
+        engine.event_queue.push(Event::new(
+            0,
+            // TODO: find better way to broadcast evens
+            usize::MAX,
+            EventPayload::Reset
+        ));
+
+        engine
     }
 
     pub fn update(&mut self) -> bool {
         if let Some(event) = self.event_queue.pop() {
+            println!("processing event: {event:?}");
+            
             self.time = event.timestamp();
             
             let target_id = event.target();
 
-            let mut ctx = ActiveContext {
-                current_time: self.time,
-                current_module_id: target_id,
-                event_queue: &mut self.event_queue,
-                cache_level_map: &mut self.cache_level_map,
-                journal: &mut self.journal
-            };
+            if target_id == usize::MAX {
+                for id in 0..self.modules.len() {
+                    let mut ctx = ActiveContext {
+                        current_time: self.time,
+                        current_module_id: id,
+                        event_queue: &mut self.event_queue,
+                        cache_level_map: &mut self.cache_level_map,
+                        journal: &mut self.journal
+                    };
+                    
+                    self.modules[id].process_event(event.clone(), &mut ctx);
+                }
+            } else if let Some(module) = self.modules.get_mut(target_id) {
+                let mut ctx = ActiveContext {
+                    current_time: self.time,
+                    current_module_id: target_id,
+                    event_queue: &mut self.event_queue,
+                    cache_level_map: &mut self.cache_level_map,
+                    journal: &mut self.journal
+                };
 
-            if let Some(module) = self.modules.get_mut(target_id) {
                 module.process_event(event, &mut ctx);
             }
 
